5bac24fd3cccb24ad601f8126070ce9ef0b5ada1
PSebastian96 <>
2023-07-19 17:52:13 +0000

    variables created for elements

--- Javascript/metronome.js
+++ Javascript/metronome.js
@@ -1,2 +1,14 @@
 import Timer from "Javascript/timer.js"
 
+// Tempo section
+const tempoInput = document.getElementById('tempo-input');
+const decreaseTempoBtn = document.getElementById('decrease-tempo');
+const increaseTempoBtn = document.getElementById('increase-tempo');
+
+// Beats per measure section
+const measureInput = document.getElementById('measure-input');
+const increaseBeats = document.getElementById('increase-measure');
+const decreaseBeats = document.getElementById('decrease-measure');;
+
+// Play - Stop button section
+const playStopBtn = document.getElementById('play-pause');
