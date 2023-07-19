f0964bf4db7a71a22e73b561a76c3c8883426c98
PSebastian96 <>
2023-07-19 17:58:14 +0000

    piano script added

---
+++ Javascript/piano.js
@@ -0,0 +1,26 @@
+const pianoKeys = document.querySelectorAll('#piano-keyboard .key');
+let volumeSlider = document.querySelector('#volume-slider input');
+let noteReveal = document.querySelector('#keys-reveal input')
+
+// audio source and passing the key the right sound 
+let audio = new Audio("pianosound/c4.mp3");
+
+const playTune = (key) => {
+    audio.src = `pianosound/${key}.mp3`;
+    audio.play();
+}
+
+pianoKeys.forEach(key => {
+    key.addEventListener('click', () => playTune(key.dataset.key))
+});
+
+const volumeSwitch = (e) => {
+    audio.volume = e.target.value;
+}
+
+const hideNotes = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+volumeSlider.addEventListener("input", volumeSwitch);
+noteReveal.addEventListener("click", hideNotes);
