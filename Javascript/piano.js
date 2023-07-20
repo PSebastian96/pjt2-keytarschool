ed57a458f39faad7cddf1058538de764e0627c99
PSebastian96 <>
2023-07-20 15:14:24 +0000

    alert to horizontal view

--- Javascript/piano.js
+++ Javascript/piano.js
@@ -2,6 +2,17 @@ const pianoKeys = document.querySelectorAll('#piano-keyboard .key');
 let volumeSlider = document.querySelector('#volume-slider input');
 let noteReveal = document.querySelector('#keys-reveal input')
 
+// alert the user to view the page horizontally
+function screenSize(pagePiano) {
+    if (pagePiano.matches) { 
+        window.alert("Turn your device horizontally for better experience.");
+    }
+  };
+  
+  let pagePiano = window.matchMedia("(max-width: 540px)")
+  screenSize(pagePiano);
+  pagePiano.addListener(screenSize);
+
 // audio source and passing the key the right sound 
 let audio = new Audio("assets/piano-sound/c4.mp3");
 
