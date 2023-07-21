cfa810ad6c4c5bed94a23c88dfa4fef260975377
PSebastian96 <>
2023-07-21 10:47:31 +0000

    play test1 passed

--- Javascript/metronome.test.js
+++ Javascript/metronome.test.js
@@ -1,13 +1,15 @@
 
 const {playClick} = ('./metronome.js');
-
 describe("playClick", () => {
-     test('count should be equal to beat', () => {
+     test('count should be 0, when the beat is changed-start a new round', () => {
         let count = 0;
         let beat = 4;
         while (count === beat) {
             count = 0;
         }
         expect(count).toBe(0);
-     })
+     });
+
+    test( '', () => {})
+
 });
